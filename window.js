2e9e7462740ed5132d0912f4475d17ae901f8b0a
Wilhelm Willie <wilhelmwillie@gmail.com>
2018-05-12 22:18:53 -0400

    Added library GIFShot, GIF conversion works but with limited functionality and customization

--- window.js
+++ window.js
@@ -37,13 +37,36 @@ $(() => {
   $('#convert-button').click(function () {
     if (vidID) {
       // Downloads YouTube video via youtubedl w/ some options
-      var video = youtubedl(
+      const video = youtubedl(
         `https://www.youtube.com/watch?v=${vidID}`,
         ['--format=18'],
         { cwd: __dirname })
 
+      // How long the video is in ms
+      let duration = 0
+
+      // Called when video download starts
+      video.on('info', function (info) {
+        duration = parseInt(info.duration * 1000)
+      })
+
       // Pipes the video download to file `temp.mp4`
       video.pipe(fs.createWriteStream('temp.mp4'))
+
+      // Create GIF from video when download is done
+      video.on('end', function () {
+        gifshot.createGIF({
+          'video': ['temp.mp4'],
+          'numFrames': (duration / 100)
+        }, function (obj) {
+          if (!obj.error) {
+            var image = obj.image
+
+            $('.result img').attr('src', image)
+            $('.result').slideDown('slow')
+          }
+        })
+      })
     }
   })
 })
